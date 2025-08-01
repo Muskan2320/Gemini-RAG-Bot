a941681792c3a75ae53447ba77888c7dfe867095
muskan2320 <muskan1802.m@gmail.com>
2025-08-01 15:38:59 +0530

    Add streamlit frontend

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import requests
+
+st.set_page_config(page_title="Gemini RAG Chatbot", layout="wide")
+st.title("Gemini RAG Chatbot")
+
+st.markdown("""
+    <style>
+    .stTextInput > div > div > input {
+        border: 2px solid red !important;
+        border-radius: 6px;
+        padding: 10px;
+        outline: none !important;
+        box-shadow: none !important;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+query = st.text_input(
+    "Ask any question related to AI technology, Generative AI, Solar cars, System hardware, or software development. We will answer based on our available data. So, what's your question:"
+)
+
+if st.button("Submit"):
+    if query.strip() == "":
+        st.warning("Please enter a question.")
+    else:
+        with st.spinner("Thinking..."):
+            try:
+                response = requests.get("http://127.0.0.1:8000/query", params={"q": query})
+                data = response.json()
+
+                st.subheader("Answer:")
+                st.write(data["answer"])
+                
+                st.subheader("Sources:")
+                for source in data["sources parsed"]:
+                    st.code(source)
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
